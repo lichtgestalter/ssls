e995c52a065aba5d68a8ebeb038f55e613ef824e
lichtgestalter <101515995+lichtgestalter@users.noreply.github.com>
2022-03-27 21:09:51 +0200

    Add files via upload

--- ssls.py
+++ ssls.py
@@ -1,74 +1,60 @@
 # -*- coding: utf-8 -*-
 # SSLS - Star System Lightcurve Simulator
-# Body motions Inspired by https://colab.research.google.com/drive/1YKjSs8_giaZVrUKDhWLnUAfebuLTC-A5
+# Body motion calculations inspired by https://colab.research.google.com/drive/1YKjSs8_giaZVrUKDhWLnUAfebuLTC-A5
 # Needs ffmpeg to convert the data into a video. Download ffmpeg from https://www.ffmpeg.org/download.html. Extract the zip file and add "<yourdriveandpath>\FFmpeg\bin" to Environment Variable PATH.
 
+def todo():
+    0
+    # Parameter für Lightcurve Zeit-Einheit einrichten.
+    # Einheiten (Watt, Sekunden) an die Lightcurve schreiben?
+    # Einheiten (AU, Meter) an eclipse- und top-view schreiben?
+    # Lightcurve Zeit-Einheit automatisch oder per Parameter in Stunden/Tage/Jahre ändern? Dictionary vielleicht so?:
+    TimeUnit = "hours"
+    time_unit = {"seconds": ('s', 1), "hours": ('h', 3600), "years": ('h', 3600 * 24 * 365.25)}
+    print("Test:", time_unit[TimeUnit])
+    # Lightcurve y-Achse Einheit SL oder Watt?
+    #
+    # Automatisch *guten* Start-Geschwindigkeitsvektor berechnen.
+    #
+    # Lernen, wie man Bahndaten aus dem Internet zieht. Dann Venus ergänzen.
+    #
+    # Sonnenflecken. Keine Gravitation, aber Positionsupdate. Position leitet sich ab aus:
+    # Position+Radius der Sonne, Rotationsgeschwindigkeit, Rotationsachse der Sonne; Längengrad, Breitengrad des Sonnenflecks.
+    #
+    # Parameter für Stern, der einen Faktor angibt, um den das Sternzentrum heller ist als der Sternrand. Noch besser: radiale Funktion statt Faktor?
+    # Dann kann ich bei Eclipse die Helligkeit an der verdeckten Sternstelle interpolieren.
+
+    # Funktion, die die Transitzeitpunkte ermittelt und speichert und Umlaufzeit-Variationen aufgrund zusätzlicher Körper erfasst.
+
+
+
+import configparser
+import math
 import matplotlib
 import matplotlib.animation
 import matplotlib.pyplot as plt
-import math
 import numpy as np
 import time
 
 
-# Parameters
-video_file = "ssls_TIC_470710327.mp4"
-
-Frames = 2500       # number of iterations/frames. Proportional to this program's run time and to the lenght of the animation.
-FPS = 30           # frames per second in video. Proportional to the velocity of the objects in the animation. Inverse proportional to lenght of video.
-DT = 1            # time difference for one iteration [s]. Proportional to the velocity of the objects in the animation. 3600 seems a good value for precise enough orbit calculations.
-Sampling_rate = 2000  # Calculating the physics is much faster than animating it. Therefore only 1/Sampling_rate of the calculated iterations is used as frame in the animation.
-Iterations = Frames * Sampling_rate
-
-G  = 6.67430e-11     # gravitational constant [m**3/kg/s**2] +/- 0.00015
-AU = 1.495978707e11  # astronomical unit [m]
-SR = 6.96342e8       # sun radius [m]
-SM = 1.98847e30      # sun mass [kg] +/- 0.00007
-SL = 3.83e26         # solar luminosity [W]
-ER = 6.378135e6      # earth radius [m]
-EM = 5.9720e24       # earth mass [kg]
-EV = 2.97852e4       # earth orbital velocity [m/s] (if orbit was circular)
-
-Scale_ecl = 0.9 * AU      # height of eclipse view plotting window in meters. Middle of window is (0.0, 0.0)
-StarScale_ecl = 1.0       # animate stars with StarScale_ecl times enlarged radius.
-PlanetScale_ecl = 100.0   # animate planets with PlanetScale_ecl times enlarged radius.
-Scale_top = 0.9 * AU      # height of top view plotting window in meters. Middle of window is (0.0, 0.0)
-StarScale_top = 1.0       # animate stars with StarScale_top times enlarged radius.
-PlanetScale_top = 100.0   # animate planets with PlanetScale_top times enlarged radius.
-
-
-def todo():
-    print("")
-    print("3-Star-System aus dem Paper nachbauen.")
-    print("Funktioniert alles auch für mehrere Sonnen und mehrere Planeten?")
-    print("")
-    print("Parameter für Stern, der einen Faktor angibt, um den das Sternzentrum heller ist als der Sternrand. Dann kann ich bei Eclipse die Helligkeit an der verdeckten Sternstelle interpolieren.")
-    print("Funktion, die die Transitzeitpunkte ermittelt und speichert und Umlaufzeit-Variationen aufgrund zusätzlicher Körper erfasst.")
-    print("")
-    print("Parameter und Options in ein File auslagern? Oder zumindest die ganzen Konstanten in init_plot als globale Parameter deklarieren.")
-    print("")
-
-
 class Body():
-    def __init__(self, name="no_name", mass=1.0, radius=1.0, brightness=0.0, luminosity=0.0, startposition=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]), color=(0.0, 0.0, 0.0), extrascale_top=1.0, extrascale_ecl=1.0):
-        self.name = name                                    # name
-        self.mass = mass                                    # [kg]
-        self.radius = radius                                # [m]
-        self.area_2d = np.pi * radius**2                    # [m**2]
-        self.brightness = brightness                        # luminosity per (apparent) area [W/m**2]
-        self.luminosity = luminosity                        # [W]
-        self.positions = np.zeros((Iterations,3))           # position for each frame
-        self.positions[0] = startposition                   # [m] initial position
-        self.velocity = velocity                            # [m/s] (np.array)
-        self.color = color                                  # (R, G, B)  each between 0 and 1
+    def __init__(self, name="no_name", mass=1.0, radius=1.0, luminosity=0.0, startposition=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]), color=(0.0, 0.0, 0.0)):
+        self.name = name                                # name
+        self.mass = mass                                # [kg]
+        self.radius = radius                            # [m]
+        self.area_2d = np.pi * radius**2                # [m**2]
+        self.luminosity = luminosity                    # [W]
+        self.brightness = luminosity / self.area_2d     # luminosity per (apparent) area [W/m**2]
+        self.positions = np.zeros((Iterations,3))       # position for each frame
+        self.positions[0] = startposition               # [m] initial position
+        self.velocity = velocity                        # [m/s] (np.array)
+        self.color = color                              # (R, G, B)  each between 0 and 1
+        if luminosity > 0:
+            extrascale_ecl, extrascale_top = StarScale_ecl, StarScale_top  # It's a star. Scale radius in plot accordingly.
+        else:
+            extrascale_ecl, extrascale_top = PlanetScale_ecl, PlanetScale_top # It's a planet. Scale radius in plot accordingly.
         self.circle_top = matplotlib.patches.Circle((0, 0), radius * extrascale_top / Scale_top)  # matplotlib patch for top view
         self.circle_ecl = matplotlib.patches.Circle((0, 0), radius * extrascale_ecl / Scale_ecl)  # matplotlib patch for eclipsed view
-
-        if self.luminosity + self.brightness > 0: # One of them is >0, so it's a star!
-            if self.luminosity > 0:
-                self.brightness = self.luminosity / self.area_2d
-            else:
-                self.luminosity = self.brightness * self.area_2d
 
 
     def eclipsed_by(self, body, frame):
@@ -88,73 +74,37 @@ class Body():
             return 0.0  # reverse eclipse: self eclipses body!
 
 
-def init_bodies(bodies):
-    bodies.append(Body(name="TIC 470710327 A",
-                       mass=6.5*SM, radius=2.0*SR,
-                       startposition=np.array([0, 3620798086, 0]), velocity=np.array([-235000, -88820, -12688.0]),
-                       luminosity=2**3.14*SL, color=(0.99, 0.99, 0.01),
-                       extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    bodies.append(Body(name="TIC 470710327 B",
-                       mass=5.9*SM, radius=1.5*SR,
-                       startposition=np.array([0, -3620798086, 0]), velocity=np.array([235000, -88820, -12688.0]),
-                       luminosity=2**3.08*SL, color=(0.01, 0.01, 0.99),
-                       extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="TIC 470710327 AohneC",
-    #                    mass=6.5*SM, radius=2.0*SR,
-    #                    startposition=np.array([0.0, 3620798086, 0.0]), velocity=np.array([-x, 0, 0.0]),
-    #                    luminosity=SL, color=(0.99, 0.99, 0.01),
-    #                    extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="TIC 470710327 BohneC",
-    #                    mass=5.9*SM, radius=1.5*SR,
-    #                    startposition=np.array([0.0, -3620798086, 0.0]), velocity=np.array([x, 0, 0.0]),
-    #                    luminosity=SL, color=(0.01, 0.01, 0.99),
-    #                    extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    bodies.append(Body(name="TIC 470710327 C",
-                       mass=15.25*SM, radius=3.5*SR,
-                       startposition=np.array([123338343881, 0, 0]), velocity=np.array([0, 85118, 12688.0]), #88820
-                       luminosity=2**4.79*SL, color=(0.99, 0.01, 0.99),
-                       extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="TIC 470710327 A+B",
-    #                    mass=12.4*SM, radius=2.0*SR,
-    #                    startposition=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]),
-    #                    luminosity=SL, color=(0.99, 0.99, 0.01),
-    #                    extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="demo1-Star1",
-    #                    mass=SM, radius=SR,
-    #                    startposition=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]),
-    #                    luminosity=SL, color=(0.99, 0.99, 0.01),
-    #                    extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="demo1-Star2",
-    #                    mass=SM*0.5, radius=SR*0.5,
-    #                    startposition=np.array([0.1*AU, -1*AU, 0.0]), velocity=np.array([-10*EV, 0.0, 0.0]),
-    #                    luminosity=SL * 0.5, color=(0.90, 0.50, 0.01),
-    #                    extrascale_ecl=StarScale_ecl, extrascale_top=StarScale_top))
-    # bodies.append(Body(name="Sun", mass=SM, radius=SR, startposition=np.array([0.0, 0.0, 0.0]), velocity=np.array([0.0, 0.0, 0.0]), color=(0.90, 0.90, 0.10), extrascale=StarScale_ecl, luminosity=SL))
-    # bodies.append(Body(name="Earth", mass=EM, radius=ER, startposition=np.array([-0.004*AU, -1.0*AU, 0.0]), velocity=np.array([-EV, 0.0, 0.0]), color=(0.20, 0.20, 0.80), extrascale=PlanetScale_ecl))
-    # bodies.append(Body(name="Earth", mass=EM, radius=ER, startposition=np.array([1.0 * AU, 0.0 * AU, 0.0 * AU]), velocity=np.array([0.0, -EV, 0.0]), color=(0.20, 0.20, 0.80), extrascale=PlanetScale_ecl))
+def init_bodies(bodies, config):
+    for section in config.sections():
+        if section not in ["Astronomical Constants", "Video", "Plot", "Scale"]:
+            bodies.append(Body(name=section,
+                               mass=eval(config.get(section, "mass")),
+                               radius=eval(config.get(section, "radius")),
+                               startposition=np.array([eval(x) for x in config.get(section, "startposition").split(",")], dtype=float),
+                               velocity=np.array([eval(x) for x in config.get(section, "velocity").split(",")], dtype=float),
+                               luminosity=eval(config.get(section, "luminosity")),
+                               color=tuple([eval(x) for x in config.get(section, "color").split(",")])))
 
 
 def init_plot(sampled_lightcurve):
     fig = plt.figure()
-    fig.set_figwidth(16)
-    fig.set_figheight(8)
-    xlim = 1.25
-    ylim = 1.0
+    fig.set_figwidth(FigureWidth)
+    fig.set_figheight(FigureHeight)
     fig.set_facecolor("black")  # background color outside of ax_eclipse and ax_lightcurve
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)  # Positions of the subplots edges, as a fraction of the figure width.
+    buffer = 0.0
+    fig.subplots_adjust(left=buffer, right=1.0-buffer, bottom=buffer, top=1-buffer)  # Positions of the subplots edges, as a fraction of the figure width.
 
     ax_eclipse = plt.subplot2grid(shape=(5, 2), loc=(0, 0), rowspan=4, colspan=1)
-
-    ax_eclipse.set_xlim(-xlim, xlim)
-    ax_eclipse.set_ylim(-ylim, ylim)
+    ax_eclipse.set_xlim(-Xlim, Xlim)
+    ax_eclipse.set_ylim(-Ylim, Ylim)
     ax_eclipse.set_aspect('equal')
     ax_eclipse.set_facecolor("black")  # background color
     # ax_eclipse.get_xaxis().set_visible(False)
     # ax_eclipse.get_yaxis().set_visible(False)
 
     ax_top = plt.subplot2grid(shape=(5, 2), loc=(0, 1), rowspan=4, colspan=1)
-    ax_top.set_xlim(-xlim, xlim)
-    ax_top.set_ylim(-ylim, ylim)
+    ax_top.set_xlim(-Xlim, Xlim)
+    ax_top.set_ylim(-Ylim, Ylim)
     ax_top.set_aspect('equal')
     ax_top.set_facecolor("black")  # background color
 
@@ -168,9 +118,11 @@ def init_plot(sampled_lightcurve):
     ax_lightcurve.set_facecolor("black")  # background color
     ax_lightcurve.tick_params(axis='x', colors='grey')  # setting up X-axis tick color to red
     ax_lightcurve.tick_params(axis='y', colors='grey')  # setting up Y-axis tick color to black
-    ax_lightcurve.plot(range(0, Iterations * DT, Sampling_rate * DT), sampled_lightcurve, color="white")
-    red_dot = matplotlib.patches.Ellipse((0,0), Iterations * DT / 200, scope / 13)  # matplotlib patch
-    red_dot.set(zorder=2)
+    ax_lightcurve.set_xlabel("time [s]")
+    ax_lightcurve.set_ylabel("luminosity [W]")
+    ax_lightcurve.plot(range(0, round(Iterations * DT), round(Sampling_rate * DT)), sampled_lightcurve, color="white")
+    red_dot = matplotlib.patches.Ellipse((0,0), Iterations * DT * RedDotWidth, scope * RedDotHeight)  # matplotlib patch
+    red_dot.set(zorder=2)  # Dot in front of lightcurve
     red_dot.set_color((1,0,0))  # red
     ax_lightcurve.add_patch(red_dot)
 
@@ -247,16 +199,16 @@ def calc_position_eclipse_luminosity(bodies, lightcurve):
             movement = body1.velocity * DT - 0.5 * acceleration * DT ** 2
             body1.positions[i] = body1.positions[i-1] + movement
         lightcurve[i] = total_luminosity(bodies, stars, i)
-        if i==1 or i % int(round(Iterations / 10)) == 0: # i > 0 and
-            # print(f'{round(i / Iterations * 100):3d}% ', end="")
-            print(f'\n{i:12d}: {distance_2d_top(bodies[0], bodies[1], i):.4e} ', end="")
+        if i % int(round(Iterations / 10)) == 0:
+            print(f'{round(i / Iterations * 100):3d}% ', end="")
+            # print(f'\n{i:12d}: {distance_2d_top(bodies[0], bodies[1], i):.4e} ', end="")
     return 0
 
 
 def update(frame, bodies, lightcurve, red_dot):
-# first parameter comes from iterator frames (a parameter of FuncAnimation)
-# the other parameters are given to this function via the parameter fargs of FuncAnimation.
-    # Update patches. Send new circle positions to animation function.
+# Update patches. Send new circle positions to animation function.
+# First parameter comes from iterator frames (a parameter of FuncAnimation).
+# The other parameters are given to this function via the parameter fargs of FuncAnimation.
     for body in bodies:  # top view: projection (x,y,z) -> (x,y), order = z
         body.circle_top.set(zorder=body.positions[frame * Sampling_rate][2])
         body.circle_top.center = body.positions[frame * Sampling_rate][0] / Scale_top, body.positions[frame * Sampling_rate][1] / Scale_top
@@ -269,39 +221,75 @@ def update(frame, bodies, lightcurve, red_dot):
 
 
 ### Main ####
-# todo()
+
+
+# read program parameters #
+config = configparser.ConfigParser(inline_comment_prefixes='#')#, interpolation=configparser.ExtendedInterpolation())
+config.read('ssls.ini')
+# [Astronomical Constants]
+G  = eval(config.get("Astronomical Constants", "G"))
+AU = eval(config.get("Astronomical Constants", "AU"))
+SR = eval(config.get("Astronomical Constants", "SR"))
+SM = eval(config.get("Astronomical Constants", "SM"))
+SL = eval(config.get("Astronomical Constants", "SL"))
+ER = eval(config.get("Astronomical Constants", "ER"))
+EM = eval(config.get("Astronomical Constants", "EM"))
+EV = eval(config.get("Astronomical Constants", "EV"))
+# [Video]
+Video_file    = config.get("Video", "Video_file")
+Frames        = eval(config.get("Video", "Frames"))
+FPS           = eval(config.get("Video", "FPS"))
+DT            = eval(config.get("Video", "DT"))
+Sampling_rate = eval(config.get("Video", "Sampling_rate"))
+Iterations    = Frames * Sampling_rate
+# [Plot]
+FigureWidth   = eval(config.get("Plot", "FigureWidth"))
+FigureHeight  = eval(config.get("Plot", "FigureHeight"))
+Xlim          = eval(config.get("Plot", "Xlim"))
+Ylim          = eval(config.get("Plot", "Ylim"))
+RedDotHeight  = eval(config.get("Plot", "RedDotHeight"))
+RedDotWidth   = eval(config.get("Plot", "RedDotWidth"))
+# [Scale]
+Scale_ecl       = eval(config.get("Scale", "Scale_ecl"))
+StarScale_ecl   = eval(config.get("Scale", "StarScale_ecl"))
+PlanetScale_ecl = eval(config.get("Scale", "PlanetScale_ecl"))
+Scale_top       = eval(config.get("Scale", "Scale_top"))
+StarScale_top   = eval(config.get("Scale", "StarScale_top"))
+PlanetScale_top = eval(config.get("Scale", "PlanetScale_top"))
+
+
+# Init
 print(f'Producing {Frames/FPS:.0f} seconds long video, covering {DT*Iterations/60/60/24:5.2f} earth days. ({DT*Sampling_rate*FPS/60/60/24:.2f} earth days per video second.)')
 lightcurve = np.zeros((Iterations))
 bodies = []
-init_bodies(bodies)
+init_bodies(bodies, config)
+# exit(888)
 
 # print(ideal_velocity(bodies[0], bodies[1]))
 # print(ideal_radius(bodies[0], bodies[1], orbital_period=1.1047*24*3600))
 # exit(1234)
 
+# Calculate body positions and the resulting lightcurve
 print(f'Calculating {Iterations:6d} iterations: ', end="")
 tic = time.perf_counter()
 calc_position_eclipse_luminosity(bodies, lightcurve)
 toc = time.perf_counter()
 print(f' 100%   {toc-tic:7.2f} seconds  ({Iterations/(toc-tic):.0f} iterations/second)')
 
-# exit(12345)
-
-sampled_lightcurve = np.take(lightcurve, range(0, Iterations, Sampling_rate))
+# Prepare animation
+sampled_lightcurve = np.take(lightcurve, range(0, Iterations, Sampling_rate))  # use only some of the calculated positions for the animation because it is so slow
 fig, ax_top, ax_eclipse, ax_lightcurve, red_dot = init_plot(sampled_lightcurve) # adjust constants inside this function to fit your screen
 for body in bodies:
     body.circle_top.set_color(body.color)
     body.circle_ecl.set_color(body.color)
     ax_top.add_patch(body.circle_top)
     ax_eclipse.add_patch(body.circle_ecl)
+
 # Rendering
 print(f'Animating {Frames:8d} frames:     ', end="")
 tic = time.perf_counter()
 anim = matplotlib.animation.FuncAnimation(fig, update, fargs=(bodies, lightcurve, red_dot), interval=1000 / FPS, frames=Frames, blit=False)
-anim.save(video_file, fps=FPS, metadata={"title": " "}, extra_args=['-vcodec', 'libx264'])
+anim.save(Video_file, fps=FPS, metadata={"title": " "}, extra_args=['-vcodec', 'libx264'])  # https://www.ffmpeg.org/libavcodec.html
 toc = time.perf_counter()
 print(f' 100%   {toc-tic:7.2f} seconds  ({Frames/(toc-tic):.0f} frames/second)')
-print(f'{video_file} saved.')
-# https://www.ffmpeg.org/libavcodec.html
-# The libavcodec library provides a generic encoding/decoding framework and contains multiple decoders and encoders for audio, video, subtitle streams and bitstream filters.
-# libx264 is a library for encoding mp4 videos.
+print(f'{Video_file} saved.')
